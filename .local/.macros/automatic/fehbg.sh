8f6f4a087c05fbe2d8aa29a0a171a1df2641f387
diego <driverag22@gmail.com>
2022-12-26 23:03:54 +0100

    new wallpaper

--- .local/.macros/automatic/fehbg.sh
+++ .local/.macros/automatic/fehbg.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-feh --no-fehbg --bg-fill '/home/diego/Pictures/wallpaper-master/1166132.jpg'
+# feh --no-fehbg --bg-fill '/home/diego/Pictures/wallpaper-master/1166132.jpg'
+feh --no-fehbg --bg-fill '/home/diego/Pictures/neon.jpg'
+# feh --no-fehbg --bg-fill '/home/diego/Downloads/neon.jpg'
+
+alias config='/usr/bin/git --git-dir=/home/diego/.dotfiles/ --work-tree=/home/diego' 
