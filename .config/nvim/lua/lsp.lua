5f4543b9dc763c3f15a1e4c7c319f9ba53957bb7
diego <driverag22@gmail.com>
2022-12-26 20:39:01 +0100

    fixed nvim deprecated error

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -95,7 +95,7 @@ cmp.setup({
   })
 
   -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 require('lspconfig')['clangd'].setup {
     capabilities = capabilities
