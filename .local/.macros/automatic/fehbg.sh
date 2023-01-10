2ed299eced37e336cef8ce320795df15ef0bf573
diego <driverag22@gmail.com>
2023-01-10 23:35:30 +0100

    added setup for second screen

--- .local/.macros/automatic/fehbg.sh
+++ .local/.macros/automatic/fehbg.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# feh --no-fehbg --bg-fill '/home/diego/Pictures/wallpaper-master/1166132.jpg'
-feh --no-fehbg --bg-fill '/home/diego/Pictures/wallpaper-master/neon.jpg'
-# feh --no-fehbg --bg-fill '/home/diego/Downloads/neon.jpg'
 
-alias config='/usr/bin/git --git-dir=/home/diego/.dotfiles/ --work-tree=/home/diego' 
+# alias config='/usr/bin/git --git-dir=/home/diego/.dotfiles/ --work-tree=/home/diego' 
+
+num_screens=$(xrandr | grep -o -c ' connected') #count num. of connected screens
+
+if [ $num_screens -eq 1 ]
+then
+    feh --no-fehbg --bg-fill '/home/diego/Pictures/wallpaper-master/neon.jpg'
+else
+    /home/diego/.local/.macros/automatic/screensNetherlands.sh
+    # feh --no-fehbg --bg-fill '/home/diego/Pictures/wallpaper-master/undefined - Imgur(1).jpg' --bg-fill '/home/diego/Pictures/wallpaper-master/neon.jpg'
+    feh --no-fehbg --bg-fill '/home/diego/Pictures/wallpaper-master/evaCatpuchin.png' --bg-fill '/home/diego/Pictures/wallpaper-master/neon.jpg'
+fi
