e483c7008f4ac14294542c3b1d4f35b0d65a2028
diego <driverag22@gmail.com>
2023-01-28 16:49:24 +0100

    re-themed battery status indicator

--- .config/rofi/applets/bin/battery.sh
+++ .config/rofi/applets/bin/battery.sh
@@ -11,9 +11,9 @@ theme="$type/$style"
 
 # Battery Info
 battery="`acpi -b | cut -d',' -f1 | cut -d':' -f1`"
-status="`acpi -b | cut -d',' -f1 | cut -d':' -f2 | tr -d ' '`"
-percentage="`acpi -b | cut -d',' -f2 | tr -d ' ',\%`"
-time="`acpi -b | awk '{print $5}' `"
+status="`cat "/sys/class/power_supply/BAT0/status" 2>&1`"
+percentage="`cat "/sys/class/power_supply/BAT0/capacity" 2>&1`"
+time="`acpi -b | grep "Battery 0" | awk '{print $5 " " $6 " " $7}' `"
 temp="`acpi -t | cut -d',' -f2`"
 voltage="`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'voltage' | awk {'print $2'}` V"
 energy="`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'energy:' | awk {'print $2'}`"
@@ -59,33 +59,44 @@ else
     ICON_CHRG=""
 fi
 
-# Discharging
-if [[ $percentage -ge 5 ]] && [[ $percentage -le 19 ]]; then
-    ICON_DISCHRG=""
-elif [[ $percentage -ge 20 ]] && [[ $percentage -le 39 ]]; then
-    ICON_DISCHRG=""
-elif [[ $percentage -ge 40 ]] && [[ $percentage -le 59 ]]; then
-    ICON_DISCHRG=""
-elif [[ $percentage -ge 60 ]] && [[ $percentage -le 79 ]]; then
-    ICON_DISCHRG=""
-elif [[ $percentage -ge 80 ]] && [[ $percentage -le 100 ]]; then
-    ICON_DISCHRG=""
-fi
-
-# Options
-layout=`cat ${theme} | grep 'USE_ICON' | cut -d'=' -f2`
-if [[ "$layout" == 'NO' ]]; then
-	option_1="$ICON_CHRG $ICON_DISCHRG ${percentage}%"
-	option_2=" $time"
-    option_3=" $temp"
-    option_4=" $voltage"
-    option_5="$energy / $energytot Wh"
+if [ $percentage -eq 100 ] 
+then
+    chargingIcon=""
+    notChargingIcon=""
+elif [ $percentage -gt 80 ]
+then
+    chargingIcon=""
+    notChargingIcon=""
+elif [ $percentage -gt 60 ]
+then
+    chargingIcon=""
+    notChargingIcon=""
+elif [ $percentage -gt 40 ]
+then
+    chargingIcon=""
+    notChargingIcon=""
+elif [ $percentage -gt 20 ]
+then
+    chargingIcon=""
+    notChargingIcon=""
 else
-	option_1="$ICON_DISCHRG"
-	option_2="$ICON_CHRG"
-	option_3=""
-	# option_4=""
+    chargingIcon=""
+    notChargingIcon=""
 fi
+   
+case "$status" in
+	"Discharging") statIcon="$notChargingIcon";;
+	"Charging") statIcon="$chargingIcon";;
+	"Not charging") statIcon="";;
+	"Unknown") statIcon="♻️ ";;
+	*) statIcon="" ;;
+esac
+
+option_1="$statIcon $percentage%"
+option_2=" $time"
+option_3=" $temp"
+option_4=" $voltage"
+option_5="$energy / $energytot Wh"
 
 # Rofi CMD
 rofi_cmd() {
@@ -101,7 +112,7 @@ rofi_cmd() {
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5" | rofi_cmd
+	echo -e "$option_1 \n $option_2\n$option_3\n$option_4\n$option_5" | rofi_cmd
 }
 
 # Execute Command
