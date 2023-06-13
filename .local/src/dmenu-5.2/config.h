8cb50245e5970d332e3fbcf4ff7baea54fad75f5
diego <driverag22@gmail.com>
2023-06-14 00:08:00 +0200

    changed dmenu colorscheme

--- .local/src/dmenu-5.2/config.h
+++ .local/src/dmenu-5.2/config.h
@@ -5,13 +5,15 @@ static int user_bh = 26;
 /* static int dmx = 7, dmy = 4; /1* put dmenu at this x, y offset *1/ */
 static int dmx = 0, dmy = 0; /* put dmenu at this x, y offset */
 /* static unsigned int dmw = 1903; /1* make dmenu this wide *1/ */
-static unsigned int dmw = 1650; /* make dmenu this wide */
+/* static unsigned int border_width = 2; /1* Size of the window border *1/ */
 static unsigned int border_width = 0; /* Size of the window border */
 
-static const char col_gray4[] = "#31333f";
+/* static const char col_gray4[] = "#31333f"; */
+static const char col_gray4[] = "#303642";
 static const char col_white[] = "#eeeeee";
-static const char col_pink[] = "#ff69b4";
-static const char col_blue[] = "#5e81ab";
+static const char col_orange[] = "#c97f71";
+/* static const char col_pink[] = "#ff69b4"; */
+static const char col_blue[] = "#81A1C1";
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -21,8 +23,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { col_white, col_gray4 },
-	[SchemeSel] = { col_gray4, col_blue},
-	[SchemeSelHighlight] = { col_white, col_blue},
+	[SchemeSel] = { col_gray4, col_orange},
+	[SchemeSelHighlight] = { col_white, col_orange},
 	[SchemeNormHighlight] = { col_blue, col_gray4 },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
