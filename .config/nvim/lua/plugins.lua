7837a2f95b52ac19804fcb98bc3cc8823acfc76a
diego <driverag22@gmail.com>
2023-06-21 17:21:14 +0200

    update transparent neovim plugin settings

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -63,7 +63,6 @@ require('onedark').setup {
     style = 'deep'
 }
 require("transparent").setup({
-  enable = true, -- boolean: enable transparent
   extra_groups = { -- table/string: additional groups that should be cleared
     -- In particular, when you set it to 'all', that means all available groups
 
@@ -75,7 +74,7 @@ require("transparent").setup({
     "BufferLineSeparator",
     "BufferLineIndicatorSelected",
   },
-  exclude = {}, -- table: groups you don't want to clear
+  exclude_groups = {}, -- table: groups you don't want to clear
 })
 require("colorizer").attach_to_buffer(0, { mode = "background", css = true})
 -- require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
