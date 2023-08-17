34240c1fa7d833e1b8dee86e0f1fa23b969f75ab
diego <driverag22@gmail.com>
2023-08-17 14:01:57 +0200

    some changes to emacs config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -8,7 +8,7 @@
  '(custom-safe-themes
    '("60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "4dcf06273c9f5f0e01cea95e5f71c3b6ee506f192d75ffda639d737964e2e14e" default))
  '(package-selected-packages
-   '(magit evil-nerd-commenter counsel-projectile company-box projectile company general which-key org-roam monokai-pro-theme evil use-package haki-theme)))
+   '(golden-ratio magit evil-nerd-commenter counsel-projectile company-box projectile company general which-key org-roam monokai-pro-theme evil use-package haki-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
