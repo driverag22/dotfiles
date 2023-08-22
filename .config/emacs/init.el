365fa6b85b39c6b1f4d0e2e40ade6d27bf49fa19
diego <driverag22@gmail.com>
2023-08-22 17:13:11 +0200

    cleaned up emacs config towards only org-mode usage (no dev work, so no things like lsp)

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -6,9 +6,12 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(doom-tokyo-night))
  '(custom-safe-themes
-   '("60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "4dcf06273c9f5f0e01cea95e5f71c3b6ee506f192d75ffda639d737964e2e14e" default))
+   '("462418b9b55cb41d878f2f30b6f533a56f644bc5576072a134db6ab2a5dc2545" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "4dcf06273c9f5f0e01cea95e5f71c3b6ee506f192d75ffda639d737964e2e14e" default))
+ '(org-agenda-files
+   '("/home/diego/org/orgRoam/20230816125732-masters.org" "/home/diego/org/orgRoam/20230821153251-emacssetupp.org" "/home/diego/org/others.org" "/home/diego/org/honors.org" "/home/diego/org/automata.org" "/home/diego/org/researchProject.org" "/home/diego/org/algorithms.org"))
+ '(org-directory "~/org")
  '(package-selected-packages
-   '(golden-ratio magit evil-nerd-commenter counsel-projectile company-box projectile company general which-key org-roam monokai-pro-theme evil use-package haki-theme)))
+   '(vulpea golden-ratio magit evil-nerd-commenter counsel-projectile company-box projectile company general which-key org-roam monokai-pro-theme evil use-package haki-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
