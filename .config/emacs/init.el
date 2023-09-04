45f3fcb7da005598f257b01f81882a3dddb956a2
diego <driverag22@gmail.com>
2023-09-04 16:43:30 +0200

    calfw (calendar) color customization

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -8,7 +8,7 @@
  '(custom-safe-themes
    '("462418b9b55cb41d878f2f30b6f533a56f644bc5576072a134db6ab2a5dc2545" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "4dcf06273c9f5f0e01cea95e5f71c3b6ee506f192d75ffda639d737964e2e14e" default))
  '(org-agenda-files
-   '("/home/diego/org/orgRoam/20230816125732-masters.org" "/home/diego/org/orgRoam/20230821153251-emacssetupp.org" "/home/diego/org/others.org" "/home/diego/org/honors.org" "/home/diego/org/automata.org" "/home/diego/org/researchProject.org" "/home/diego/org/algorithms.org"))
+   '("/home/diego/org/orgRoam/20230816125732-masters.org" "/home/diego/org/orgRoam/20230821153251-emacssetupp.org"))
  '(org-directory "~/org")
  '(package-selected-packages
    '(calfw-org calfw vulpea golden-ratio evil-nerd-commenter projectile general which-key org-roam monokai-pro-theme evil use-package haki-theme)))
