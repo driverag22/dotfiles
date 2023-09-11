b7d06de097405f31863e8ba5a5c1cffc1f267a1e
diego <driverag22@gmail.com>
2023-09-11 13:37:50 +0200

    added more capture templates

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -6,12 +6,12 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(doom-tokyo-night))
  '(custom-safe-themes
-   '("462418b9b55cb41d878f2f30b6f533a56f644bc5576072a134db6ab2a5dc2545" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "4dcf06273c9f5f0e01cea95e5f71c3b6ee506f192d75ffda639d737964e2e14e" default))
+   '("b6269b0356ed8d9ed55b0dcea10b4e13227b89fd2af4452eee19ac88297b0f99" "c8b83e7692e77f3e2e46c08177b673da6e41b307805cd1982da9e2ea2e90e6d7" "462418b9b55cb41d878f2f30b6f533a56f644bc5576072a134db6ab2a5dc2545" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "4dcf06273c9f5f0e01cea95e5f71c3b6ee506f192d75ffda639d737964e2e14e" default))
  '(org-agenda-files
    '("/home/diego/org/orgRoam/20230816125732-masters.org" "/home/diego/org/orgRoam/20230821153251-emacssetupp.org"))
  '(org-directory "~/org")
  '(package-selected-packages
-   '(visual-fill calfw-org calfw vulpea golden-ratio evil-nerd-commenter projectile general which-key org-roam monokai-pro-theme evil use-package haki-theme)))
+   '(affe calfw-org calfw org-roam-ui visual-fill vulpea golden-ratio evil-nerd-commenter projectile general which-key org-roam monokai-pro-theme evil use-package haki-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
