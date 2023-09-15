5d7f14df950ebabf32061c115ac0394e0c72ec69
diego <driverag22@gmail.com>
2023-09-15 09:10:37 +0200

    smaller border

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -2,7 +2,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 7;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -11,8 +11,8 @@ static const int vertpad            = 3;       /* vertical padding of bar */
 static const int sidepad            = 7;       /* horizontal padding of bar */
 static const int user_bh            = 22;
 static const char *fonts[]          = { "JetBrains Mono:size=12", "DejaVu Sans Mono wifi ramp:size=11"};
-static const char col_bg[]    = "#222436";
-static const char col_bluer[]      = "#6d86b8";
+static const char col_bg[]          = "#222436";
+static const char col_bluer[]       = "#6d86b8";
 static const char col_lblue[]       = "#b1bedb";
 
 static const char *colors[][3]      = {
