e29108e34dbbb661d5bdd9dde31c1b5d65e08321
diego <driverag22@gmail.com>
2024-04-09 11:42:11 +0200

    changed emacs capture templates, added G back to memory on dwmBar, temp. changed zathura colorscheme

--- .local/.macros/dwmBar/runners/memory.sh
+++ .local/.macros/dwmBar/runners/memory.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 TMP="$HOME/.local/.macros/dwmBar/tmp"
 while true; do
-    mem=$(free -h --kilo | awk '/^Mem:/ {gsub(/[A-Za-z]/, "", $3) gsub(/[A-Za-z]/, "", $2); print $3 "/" $2}')
+    # mem=$(free -h --kilo | awk '/^Mem:/ {gsub(/[A-Za-z]/, "", $3) gsub(/[A-Za-z]/, "", $2); print $3 "/" $2}')
+    mem=$(free -h --kilo | awk '/^Mem:/ {gsub(/[A-Za-z]/, "", $3); print $3 "/" $2}')
     echo "$mem" > "$TMP/mem.txt"
     sleep 5s
 done &
