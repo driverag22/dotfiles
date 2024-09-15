44339a3f4f81cb5dbcaad6908c2e254105ba81cd
diego <driverag22@gmail.com>
2024-09-15 10:21:18 +0200

    updated wallpaper setter for multiple screens

--- .local/.macros/automatic/fehbg.sh
+++ .local/.macros/automatic/fehbg.sh
@@ -4,9 +4,8 @@ num_screens=$(xrandr | grep -o -c ' connected') #count num. of connected screens
 
 if [ $num_screens -eq 1 ]
 then
-    # feh --no-fehbg --bg-fill '/home/diego/Pictures/wallpaper-master/SDAGvuq.png'
     feh --no-fehbg --bg-fill '/home/diego/Pictures/wallpaper-master/wallpaper.png'
 else
     /home/diego/.local/.macros/automatic/screensNetherlands.sh
-    feh --no-fehbg --bg-fill '/home/diego/Pictures/wallpaper-master/SDAGvuq.png' --bg-fill '/home/diego/Pictures/wallpaper-master/SDAGvuq.png'
+    feh --no-fehbg --bg-fill '/home/diego/Pictures/wallpaper-master/wallpaper.png' --bg-fill '/home/diego/Pictures/wallpaper-master/wallpaper.png'
 fi
