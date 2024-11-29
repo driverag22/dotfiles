8e6e9ccfb9154f9a16d0cf785900f65f0c34c521
diego <driverag22@gmail.com>
2024-11-29 15:37:56 +0100

    fixed small mistake

--- .config/systemd/user/check-battery.sh
+++ .config/systemd/user/check-battery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # printf 'battery check script started' | systemd-cat -t check-battery
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | awk 'print int($4)}'` < 20 ]] ; then
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | awk '{print int($4)}'` < 20 ]] ; then
         # printf 'condition is true' | systemd-cat -t check-battery
         BATTINFO_CLEAN=$(echo "$BATTINFO" | awk -F', ' '{print $2 ", " $3}')
         DISPLAY=:0 /usr/bin/notify-send -u critical "Battery low!" "$BATTINFO_CLEAN"
