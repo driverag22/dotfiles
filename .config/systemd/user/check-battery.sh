a77d0530582689734770415c520722e876ac88b4
diego <driverag22@gmail.com>
2024-12-03 15:46:16 +0100

    small fix for when battery is 'not charging'

--- .config/systemd/user/check-battery.sh
+++ .config/systemd/user/check-battery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # printf 'battery check script started' | systemd-cat -t check-battery
-BATTINFO=`acpi -b`
+BATTINFO=`acpi -b | head -n 1`
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | awk '{print int($4)}'` < 20 ]] ; then
         # printf 'condition is true' | systemd-cat -t check-battery
         BATTINFO_CLEAN=$(echo "$BATTINFO" | awk -F', ' '{print $2 ", " $3}')
