78ad244df3701e0aa6610631115db4e295809c3c
diego <driverag22@gmail.com>
2025-02-19 13:49:15 +0100

    added rainbow tags patch

--- .local/src/dwm/dwm.c
+++ .local/src/dwm/dwm.c
@@ -274,6 +274,7 @@ static int running = 1;
 static Bool restart = False;
 static Cur *cursor[CurLast];
 static Clr **scheme;
+static Clr **tagscheme;
 static Display *dpy;
 static Drw *drw;
 static Monitor *mons, *selmon;
@@ -734,7 +735,8 @@ drawbar(Monitor *m)
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
 		w = TEXTW(tags[i]);
-		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
+		/* drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]); */
+        drw_setscheme(drw, (m->tagset[m->seltags] & 1 << i ? tagscheme[i] : scheme[SchemeNorm]));
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		if (occ & 1 << i) {
 			drw_rect(drw, x + boxw, 18, w - 2 * boxw - 1, boxw - 1,
@@ -1631,9 +1633,14 @@ setup(void)
 	cursor[CurResize] = drw_cur_create(drw, XC_sizing);
 	cursor[CurMove] = drw_cur_create(drw, XC_fleur);
 	/* init appearance */
+    if (LENGTH(tags) > LENGTH(tagsel))
+	    die("too few color schemes for the tags");
 	scheme = ecalloc(LENGTH(colors), sizeof(Clr *));
 	for (i = 0; i < LENGTH(colors); i++)
 		scheme[i] = drw_scm_create(drw, colors[i], 3);
+    tagscheme = ecalloc(LENGTH(tagsel), sizeof(Clr *));
+    for (i = 0; i < LENGTH(tagsel); i++)
+	    tagscheme[i] = drw_scm_create(drw, tagsel[i], 2);
 	/* init bars */
 	updatebars();
 	updatestatus();
