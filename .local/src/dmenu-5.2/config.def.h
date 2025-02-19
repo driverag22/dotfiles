6fe1e1283f713c372db189e0a8ec88a8a98a0494
diego <driverag22@gmail.com>
2025-02-19 14:10:39 +0100

    updated dmenu and alacritty theme to quasimoto theme

--- .local/src/dmenu-5.2/config.def.h
+++ .local/src/dmenu-5.2/config.def.h
@@ -7,10 +7,11 @@ static int user_bh = 21;
 /* static unsigned int border_width = 2; /1* Size of the window border *1/ */
 static unsigned int border_width = 0; /* Size of the window border */
 
-static const char col_bg[]    = "#1A1E25";
-static const char col_white[] = "#eeeeee";
-static const char col_bBlue[] = "#338fff";
-static const char col_orange[]      = "#C97F71";
+static const char col_bg[]          = "#020202";
+static const char col_white[]       = "#eeeeee";
+static const char col_pink[]        = "#CA509C";
+static const char col_cyan[]        = "#32A9A6";
+
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static const char *fonts[] = {
 	"JetBrains Mono:size=13"
@@ -19,10 +20,10 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { col_white, col_bg},
-	[SchemeSel] = { col_bg, col_orange},
-	[SchemeSelHighlight] = { col_white, col_orange},
-	[SchemeNormHighlight] = { col_bBlue, col_bg },
-	[SchemeOut] = { col_bg, "#00ffff" },
+	[SchemeSel] = { col_bg, col_pink},
+	[SchemeSelHighlight] = { col_white, col_pink},
+	[SchemeNormHighlight] = { col_cyan, col_bg },
+	[SchemeOut] = { col_bg, col_cyan },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
