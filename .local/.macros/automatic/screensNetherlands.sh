dbe6b2694c79fc4bad2e9a122785dc06aa99ba01
diego <driverag22@gmail.com>
2025-03-03 10:59:52 +0100

    fixed screen script

--- .local/.macros/automatic/screensNetherlands.sh
+++ .local/.macros/automatic/screensNetherlands.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 2560x62 --rotate normal --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+xrandr --output eDP1 --mode 1920x1080 --pos 2560x360 --rotate normal --output VIRTUAL1 --off --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal
